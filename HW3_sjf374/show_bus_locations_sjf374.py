edd38079c8f1d481e3922048e61f19b2e5e04f03
samjfalk <samjfalk@gmail.com>
2018-09-26 08:49:13 -0400

    add error handling

--- HW3_sjf374/show_bus_locations_sjf374.py
+++ HW3_sjf374/show_bus_locations_sjf374.py
@@ -5,13 +5,14 @@ try:
 except ImportError:
     import urllib.request as urllib
 
+# if you add more than one word as argument it will give you an error as well--from class
+if not len(sys.argv) == 3:
+    print("Invalid number of arguments. Run as: python show_bus_locations_sjf374.py <MTA_KEY> <BUS_LINE>")
+    sys.exit()
 
 #choose the first input as key and the second one as the bus
 key = sys.argv[1]
 bus = sys.argv[2]
-
-
-
 
 def bus_location(key, bus):
 	#in case it is input as lower case-- mta does not recognize it
@@ -26,7 +27,6 @@ def bus_location(key, bus):
 	data = response.read().decode("utf-8")
 	parsedjson = json.loads(data, parse_constant= True)
 
-
 	#filter down dictionaries to dictionary with vehicle activity
 	busactivity = parsedjson['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
 
@@ -36,11 +36,10 @@ def bus_location(key, bus):
 	print ('Number of Active Buses : ' + str(len(busactivity)))
 
 	#show each bus and their corresponding location
-	i = 0
+	i = 1
 	for x in busactivity:
 	    print ('Bus %s is at latitude %s and longitude %s' \
 	    	% (i, str(x['MonitoredVehicleJourney']['VehicleLocation']['Latitude']), str(x['MonitoredVehicleJourney']['VehicleLocation']['Longitude'])))
 	    i+=1
-
 
 bus_location(key, bus)
